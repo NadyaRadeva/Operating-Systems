108f34ff0de7dc79b0d349a8ecef3331ed1ff3a0
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2026-03-31 01:03:25 +0300

    Create commands.sh

---
+++ 03. pipes/03-a-2000/commands.sh
@@ -0,0 +1,6 @@
+s0600454@astero:~$ head -n 12 /etc/passwd
+s0600454@astero:~$ head -c 26 /etc/passwd
+s0600454@astero:~$ head -n -4 /etc/passwd
+s0600454@astero:~$ tail -n 17 /etc/passwd
+s0600454@astero:~$ head -n 151 /etc/passwd | tail -n 1
+s0600454@astero:~$ head -n 13 /etc/passwd | tail -n 1 | tail -c 4
