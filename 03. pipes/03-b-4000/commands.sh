000271b917db805989aef13b04351aaad95fb12f
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2026-04-08 13:24:48 +0300

    Create commands.sh
    Added commands to create a directory and files, edit them, and count lines and characters.

---
+++ 03. pipes/03-b-4000/commands.sh
@@ -0,0 +1,10 @@
+s0600454@astero:~$ mkdir dir5
+s0600454@astero:~$ touch dir5/file1, touch dir5/file2, touch dir5/file3
+s0600454@astero:~$ vi dir5/file1
+s0600454@astero:~$ vi dir5/file2
+s0600454@astero:~$ vi dir5/file3
+s0600454@astero:~$ wc dir5/file1
+s0600454@astero:~$ wc dir5/file2
+s0600454@astero:~$ wc dir5/file3
+s0600454@astero:~$ wc -l -m dir5/file1 dir5/file2 dir5/file3
+s0600454@astero:~$ wc -l dir5/file1 dir5/file2 dir5/file3 | tail -1
